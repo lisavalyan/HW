a746104e889a8dd698656e873336bf5e1bbe3953
elizaveta <lizanaj3@gmail.com>
2022-06-26 16:28:06 +0700

    4 дз

---
+++ Sem4/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+Console.WriteLine("Введите первое число");
+int num1=Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+int num2=Convert.ToInt32(Console.ReadLine());
+
+int current=1;
+int n=1;
+int result=num1;
+
+while(current<=num2)
+{
+n=n*result;
+result=1*num1;
+current++;
+}
+
+Console.WriteLine($"={n}");
+*/
+
