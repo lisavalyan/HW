0b1031be9124e5624f2ad6a3698212ac7c7dc0c2
elizaveta <lizanaj3@gmail.com>
2022-07-03 22:09:36 +0700

    дз 4

--- Sem4/Program.cs
+++ Sem4/Program.cs
@@ -27,26 +27,3 @@ int num=Convert.ToInt32(Console.ReadLine());
 
  Console.Write("[" + num + "]" );
 
-/*
-int size=8;
-int[] array=new int[size];
-
-int i=0;
-
-while( i < size)
-{
-       i++; 
-
-    num = num % 10 ;
-
-    array[i]=num;
-
-}
-
-for( i = 0 ; i < size ; i++ )
-{
-
-    Console.Write( + array[i] + "," );
-
-}
-*/
