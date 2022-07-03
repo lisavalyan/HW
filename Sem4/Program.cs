74d5d95b338bff58dc5a684b8d5c37f2e8f8458f
elizaveta <lizanaj3@gmail.com>
2022-07-03 22:06:37 +0700

    4 дз

--- Sem4/Program.cs
+++ Sem4/Program.cs
@@ -1,4 +1,4 @@
-﻿
+﻿/*
 Console.WriteLine("Введите первое число");
 int num1=Convert.ToInt32(Console.ReadLine());
 
@@ -10,6 +10,7 @@ int n=1;
 int result=num1;
 
 while(current<=num2)
+
 {
 n=n*result;
 result=1*num1;
@@ -17,3 +18,35 @@ current++;
 } 
 
 Console.WriteLine($"={n}");
+*/
+
+
+
+Console.WriteLine("Введите 8 цифр");
+int num=Convert.ToInt32(Console.ReadLine());
+
+ Console.Write("[" + num + "]" );
+
+/*
+int size=8;
+int[] array=new int[size];
+
+int i=0;
+
+while( i < size)
+{
+       i++; 
+
+    num = num % 10 ;
+
+    array[i]=num;
+
+}
+
+for( i = 0 ; i < size ; i++ )
+{
+
+    Console.Write( + array[i] + "," );
+
+}
+*/
