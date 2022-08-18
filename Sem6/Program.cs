e79a1b2e13f0cd697047b105b8c971a65c3d0650
elizaveta <lizanaj3@gmail.com>
2022-08-18 13:03:48 +0700

    6 дз

---
+++ Sem6/Program.cs
@@ -0,0 +1,34 @@
+﻿/*
+Console.WriteLine("Введите числа ");
+int[] array = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int count = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > 0)
+
+    {
+        count++;
+    }
+
+}
+ 
+Console.WriteLine($"Колличество положительных чисел {count}");
+*/
+
+
+
+
+
+
+    
+        int[] array = { 7, 2, 6, 0, 1, 4 };
+ 
+        int[] copyarr = new int[array.Length];
+        for (int i = 0; i < array.Length; i++)
+       {
+            copyarr[i] = array[i];
+        }
+ 
+        Console.WriteLine(String.Join(", ", copyarr));     
+    
